e4b7aa9cefdf933f2f5706ab79a7346a4fc4fedd
Jose3004 <119958493+Jose3004@users.noreply.github.com git config --global user.email "119958493+Jose3004@users.noreply.github.com>
2024-06-01 22:06:37 -0500

    Configuración general  del proyecto Node JS

---
+++ src/database/query.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE playstation;
+
+USE playstation;
+
+CREATE TABLE juegos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    GAME VARCHAR(50),
+    CONSOLE VARCHAR(50),
+    PUBLISHER VARCHAR(50),
+    RELEASE_DATE INT,
+    PEGI_INFO VARCHAR(50)
+);
+
+SELECT * FROM playstation;
+
